eee6b3a0bd7e22a8078132a3af3681284e6bda42
nandobfer <nandobfer@yahoo.com.br>
2023-08-11 16:27:19 -0300

    update: message limit fetching

--- src/io/zap.ts
+++ src/io/zap.ts
@@ -19,7 +19,7 @@ const sync = async (socket: Socket, clients: ClientBag) => {
 
 const getChat = async (socket: Socket, _chat: any) => {
     const chatObj = await zap.client.getChatById(_chat.id._serialized)
-    const messages = await chatObj.fetchMessages({})
+    const messages = await chatObj.fetchMessages({ limit: 100 })
     const contact = await chatObj.getContact()
     const profilePic = await contact.getProfilePicUrl()
 
