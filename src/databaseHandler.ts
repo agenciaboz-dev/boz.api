026d347ae0df2ae263a507f26904c2b68c0b8622
nandobfer <nandobfer@yahoo.com.br>
2023-08-24 10:30:44 -0300

    test: logs

--- src/databaseHandler.ts
+++ src/databaseHandler.ts
@@ -180,15 +180,16 @@ const log = {
     },
 
     list: {
-        status: async () => {
-            const total = await prisma.statusLog.count()
-            const count = Array.from({ length: Math.floor(total / 100) + 1 }, (_, i) => i)
-            const batch = 100
-            const list = await Promise.all(
-                count.map(async (index) => await prisma.statusLog.findMany({ include: inclusions.logs, skip: index * batch, take: batch }))
-            )
-            return list.flat()
-        },
+        status: async () => await prisma.statusLog.findMany({ include: inclusions.logs }),
+        // status: async () => {
+        //     const total = await prisma.statusLog.count()
+        //     const count = Array.from({ length: Math.floor(total / 100) + 1 }, (_, i) => i)
+        //     const batch = 100
+        //     const list = await Promise.all(
+        //         count.map(async (index) => await prisma.statusLog.findMany({ include: inclusions.logs, skip: index * batch, take: batch }))
+        //     )
+        //     return list.flat()
+        // },
     },
 }
 
