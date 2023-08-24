9a7573964a9793736032bc124a3d446e4c0c0fd3
nandobfer <nandobfer@yahoo.com.br>
2023-08-24 10:18:03 -0300

    Revert "update: natching fetch logs list"
    This reverts commit d6a1957530527c9c5193f23d800e5261d3658bec.

--- src/databaseHandler.ts
+++ src/databaseHandler.ts
@@ -101,23 +101,7 @@ const department = {
 }
 
 const role = {
-    list: async () => {
-        const total = await prisma.role.count()
-        const count = Array.from({ length: Math.floor(total / 100) + 1 }, (_, i) => i)
-        const batch = 100
-        const list = await Promise.all(
-            count.map(
-                async (index) =>
-                    await prisma.statusLog.findMany({
-                        include: inclusions.logs,
-                        orderBy: { id: "asc" },
-                        skip: index * batch,
-                        take: batch,
-                    })
-            )
-        )
-        return list.flat()
-    },
+    list: async () => await prisma.role.findMany({ include: inclusions.role }),
     new: async (role: NewServiceForm) => await prisma.role.create({ data: { name: role.name, tag: role.tag }, include: inclusions.role }),
 }
 
