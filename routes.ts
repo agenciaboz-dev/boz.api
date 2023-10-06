08c127d2f7493f409bcb332dfca0b70cc1b6e25b
nandobfer <nandobfer@yahoo.com.br>
2023-10-06 13:36:17 -0300

    feat: added zapzap endpoint

--- routes.ts
+++ routes.ts
@@ -2,9 +2,11 @@ import express, { Express, Request, Response } from "express"
 import user from "./src/user"
 import customer from "./src/customer"
 import department from "./src/department"
+import whatsapp from "./src/rest/whatsapp"
 
 export const router = express.Router()
 
 router.use("/user", user)
 router.use("/customer", customer)
 router.use("/department", department)
+router.use("/whatsapp", whatsapp)
