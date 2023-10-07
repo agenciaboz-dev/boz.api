b59b2069527e24ee0c5850d06389a69395c6f5fe
nandobfer <nandobfer@yahoo.com.br>
2023-10-07 11:38:22 -0300

    update: handling pagseguro error

--- src/rest/pagseguro.ts
+++ src/rest/pagseguro.ts
@@ -3,11 +3,14 @@ import { PrismaClient } from '@prisma/client'
 const router = express.Router()
 const prisma = new PrismaClient()
 
-router.post('/', async (request:Request, response:Response) => {    
-    const data = request.body
+router.post("/", async (request: Request, response: Response) => {
+    try {
+        const data = request.body
 
-    console.log({ order: data })
-
+        console.log({ order: data })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 export default router
