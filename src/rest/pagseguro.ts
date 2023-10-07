03188fec7ba1d9fe11bd3f4872f7bea6bc3b7626
nandobfer <nandobfer@yahoo.com.br>
2023-10-07 01:02:48 -0300

    feat: added pagseguro endpoint

---
+++ src/rest/pagseguro.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+const router = express.Router()
+const prisma = new PrismaClient()
+
+router.post('/', async (request:Request, response:Response) => {    
+    const data = request.body
+
+    console.log(data)
+
+})
+
+export default router
