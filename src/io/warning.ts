bab1c5226123139d766e07567836aa0554781326
nandobfer <nandobfer@yahoo.com.br>
2023-10-11 15:14:07 -0300

    update: io emition

--- src/io/warning.ts
+++ src/io/warning.ts
@@ -1,12 +1,15 @@
 import { Warning } from "@prisma/client"
 import { Socket } from "socket.io"
 import databaseHandler from "../databaseHandler"
+import { getIoInstance } from "./socket"
 
 const create = async (socket: Socket, data: NewWarningForm) => {
+    const io = getIoInstance()
+
     try {
         const warning = await databaseHandler.warning.create(data)
         socket.emit("warning:new:success", warning)
-        socket.broadcast.emit("warning:new", warning)
+        io.emit("warning:new", warning)
     } catch (error) {
         console.log(error)
         socket.emit("warning:new:error", error)
