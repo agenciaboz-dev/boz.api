c67050bdfbb82f5f49c5eb9a76de7cc1e687ba13
Lucas Patricio <lpatriciopro@outlook.com>
2023-11-30 13:30:06 -0300

    databaseHandler - index.ts criado e atualizado

---
+++ src/databaseHandler/index.ts
@@ -0,0 +1,21 @@
+import apiTester from "./apiTester";
+import customer from "./customer";
+import department from "./department";
+import log from "./log";
+import qrcode from "./qrcode";
+import role from "./role";
+import service from "./service";
+import user from "./user";
+import warnings from "./user";
+
+export default {
+  apiTester,
+  customer,
+  department,
+  log,
+  qrcode,
+  role,
+  service,
+  user,
+  warnings,
+};
