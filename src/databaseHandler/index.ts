4598b40300aa22b4584ee6b89c451ba39aa2adc9
nandobfer <nandobfer@yahoo.com.br>
2023-11-30 16:11:07 -0300

    update: theme

--- src/databaseHandler/index.ts
+++ src/databaseHandler/index.ts
@@ -1,21 +1,23 @@
-import apiTester from "./apiTester";
-import customer from "./customer";
-import department from "./department";
-import log from "./log";
-import qrcode from "./qrcode";
-import role from "./role";
-import service from "./service";
-import user from "./user";
-import warning from "./warning";
+import apiTester from "./apiTester"
+import customer from "./customer"
+import department from "./department"
+import log from "./log"
+import qrcode from "./qrcode"
+import role from "./role"
+import service from "./service"
+import user from "./user"
+import warning from "./warning"
+import theme from "./theme"
 
 export default {
-  apiTester,
-  customer,
-  department,
-  log,
-  qrcode,
-  role,
-  service,
-  user,
-  warning,
-};
+    apiTester,
+    customer,
+    department,
+    log,
+    qrcode,
+    role,
+    service,
+    user,
+    warning,
+    theme
+}
---
+++ src/databaseHandler/theme.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const include = { user: true }
+
+const create = async (data: NewTheme) =>
+    await prisma.theme.create({
+        data: {
+            userId: data.userId,
+            name: data.name,
+            primary: data.primary,
+            secondary: data.secondary,
+            terciary: data.terciary,
+            success: data.success,
+            warning: data.warning,
+            background_primary: data.background.primary,
+            background_secondary: data.background.secondary,
+            text_primary: data.text.primary,
+            text_secondary: data.text.secondary,
+            text_terciary: data.text.terciary,
+            timestamp: new Date().getTime().toString()
+        },
+        include
+    })
+const list = async () => await prisma.theme.findMany({ include })
+const activate = async (id: number) => prisma.theme.update({ where: { id }, data: { active: true }, include })
+const deactivate = async () => prisma.theme.updateMany({ data: { active: false } })
+const update = async (id: number, data: NewTheme) =>
+    await prisma.theme.update({
+        where: { id },
+        data: {
+            name: data.name,
+            primary: data.primary,
+            secondary: data.secondary,
+            terciary: data.terciary,
+            success: data.success,
+            warning: data.warning,
+            background_primary: data.background.primary,
+            background_secondary: data.background.secondary,
+            text_primary: data.text.primary,
+            text_secondary: data.text.secondary,
+            text_terciary: data.text.terciary
+        },
+        include
+    })
+
+export default { create, list, activate, deactivate, update }
