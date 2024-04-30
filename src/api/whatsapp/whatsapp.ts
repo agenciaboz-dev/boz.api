fb54f638415fce82dc56afb21fe226148fb9a42d
nandobfer <nandobfer@yahoo.com.br>
2024-04-30 17:31:25 -0300

    update: zap token

--- src/api/whatsapp/whatsapp.ts
+++ src/api/whatsapp/whatsapp.ts
@@ -1,13 +1,13 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { WhatsappApiForm, WhatsappForm } from "../../definitions/WhatsappForm"
 import { writeFileSync } from "fs"
 
-const MAX_MESSAGES_BATCH = 250
-const INTERVAL = 3 * 60 * 60 * 1000
+const MAX_MESSAGES_BATCH = 50
+const INTERVAL = 1000 * 60 * 60
 // const INTERVAL = 20 * 1000
 
 const token =
-    "EAAEhE4xyg9cBOZBP65vtjnnDfUmlnfB2bC5s9zWjIY9WkqoZAN73tWRCYcob2MDfpo1zg8LtCNEHKtKhTsYZBJb9sHO7aNdFCZAyOL1pVYpPdvWxKud1H8GoakFhm3CCY1UvWfLMDZBpjqpadqWVlHPiYsDaa31DnqPcV0IOzaIfbzzEcB9Ge1BWZBtS7Xc9O4oROZBHVcAZBpvNZBSefEF3S7JZAv541V"
+    "EAAEhE4xyg9cBO5JyT4rNPYB897H3THbQz6slZBBPUEQ0OaPpumZCIGhzL8rcfZAEZBoYUkcLaZA77PYOXLAs2G6aqzbosGbKBlJGMPRPaVRLEUxOmseO2nFo3egWPk8jCl8te0ZBCxv5ehHwbD4iLiTSY4r5nbjrtbPCXI7yfyVE8xggg3kdnSYZAbrlU6HurIQy66PpZBm7GWcDCBmb5cCMkzlJrWQZD"
 
 export const api = axios.create({
     baseURL: "https://graph.facebook.com/v19.0/306058415918366",
@@ -46,10 +46,17 @@ const sendNextMessage = async () => {
         }
         try {
             const whatsapp_response = await api.post("/messages", form)
+            console.log(whatsapp_response.data)
             const log = `${new Date().toLocaleString("pt-br")}\n` + JSON.stringify(whatsapp_response.data) + "\n\n"
             writeFileSync("whatsapp_sent.log", log, { flag: "a+" })
         } catch (error) {
-            console.log(error)
+            if (error instanceof AxiosError) {
+                console.log(error.response?.data)
+                const log = `${new Date().toLocaleString("pt-br")}\n` + JSON.stringify(error.response?.data) + "\n\n"
+                writeFileSync("whatsapp_error.log", log, { flag: "a+" })
+            } else {
+                console.log(error)
+            }
         }
     }
 }
@@ -66,4 +73,5 @@ const sendBatch = async () => {
     }
 }
 
+setTimeout(() => sendBatch(), 1000 * 60)
 setInterval(() => sendBatch(), INTERVAL)
